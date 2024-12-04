cf5e992ba9154b0cb19e5da2cd3da69125d4fffc
Magicmike112 <bigmike@hawaiiantel.net>
2024-12-04 00:15:17 -1000

    add content.html

--- projects/final/process_form.php
+++ projects/final/process_form.php
@@ -1,1 +1,18 @@
-<form action="backend/process_form.php" method="post">
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $order = htmlspecialchars($_POST['order']);
+    $pickup = htmlspecialchars($_POST['pickup']);
+
+    // You can process the data, for example, send an email or store it in a database
+
+    // For now, let's just output the submitted data
+    echo "Thank you for contacting us, $name!<br>";
+    echo "We will reach out to you at $email.<br>";
+    echo "Order Inquiry: $order<br>";
+    echo "Preferred Pickup Date: $pickup<br>";
+} else {
+    echo "Form was not submitted properly.";
+}
+?>
